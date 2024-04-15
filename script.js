e9e8c292706f58fce6b19cfe9646660e6758b7dc
lasarinii <pedroluislasarini23@gmail.com>
2024-04-15 13:55:21 -0300

    erro, now push

--- script.js
+++
@@ -1,7 +0,0 @@
-var pswd = ""
-document.querySelector('input').addEventListener('input', (e) => {
-    pswd = e.target.value
-})
-document.querySelector('button').addEventListener('click', () => {
-    window.location.href = '/' + pswd
-})
