31c5cd7cff9c43fe725ea35a2c36a6e9fb7dba96
lasarinii <pedroluislasarini23@gmail.com>
2024-04-15 14:03:56 -0300

    vercel?

---
+++ api/index.go
@@ -0,0 +1,49 @@
+package handler
+
+import(
+    "log"
+    "net/http"
+    "os"
+    "text/template"
+    validator "github.com/wagslane/go-password-validator"
+)
+
+type Entropy struct {
+    Password string
+    Value float64
+    Message string
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+    t, err := template.ParseFiles("../index.tmpl")
+    if err != nil {
+        log.Println("Error parsing template:", err)
+        os.Exit(1)
+    }
+
+    pswd := r.PathValue("password")
+    if pswd == "" {
+        pswd = "password"
+    }
+
+    err = validator.Validate(pswd, 60)
+
+    var msg string
+    if err != nil {
+        msg = err.Error()
+    } else {
+        msg = "Suficient entropy"
+    }
+
+    result := Entropy{
+        Password: pswd,
+        Value: validator.GetEntropy(pswd),
+        Message: msg,
+    }
+
+    w.Header().Add("Content-Type", "text/html")
+
+    t.ExecuteTemplate(w, "index", map[string]interface{}{
+        "Content": result,
+    })
+}
